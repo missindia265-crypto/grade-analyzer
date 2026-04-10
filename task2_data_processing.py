09d3b783893facc4aa8c1df68c48a6b34e6bd710
missindia265-crypto <missindia265@gmail.com>
2026-04-10 08:08:29 -0700

    Add files via upload

---
+++ task2_data_processing.py
@@ -0,0 +1,47 @@
+post_id,title,category,score,num_comments,author,collected_at
+47676174,Mysteries of Dropbox: Property-Based Testing of a Distributed Sync Service [pdf],technology,32,5,JackeJR,2026-04-10 14:52:05
+47716809,"Intel 486 CPU announced April 10, 1989",technology,97,72,jnord,2026-04-10 14:52:05
+47716490,FBI used iPhone notification data to retrieve deleted Signal messages,technology,244,100,01-_-,2026-04-10 14:52:06
+47704804,How NASA built Artemis II’s fault-tolerant computer,technology,501,190,speckx,2026-04-10 14:52:06
+47713495,A new trick brings stability to quantum operations,technology,187,45,joko42,2026-04-10 14:52:07
+47717587,OpenAI backs Illinois bill that would limit when AI labs can be held liable,technology,312,204,smurda,2026-04-10 14:52:07
+47712718,I still prefer MCP over skills,technology,313,264,gmays,2026-04-10 14:52:07
+47674894,Model-Based Testing for Dungeons & Dragons,technology,75,24,Firfi,2026-04-10 14:52:08
+47717973,The effects of caffeine consumption do not decay with a ~5 hour half-life,technology,43,29,swah,2026-04-10 14:52:08
+47708818,Native Instant Space Switching on macOS,technology,579,280,PaulHoule,2026-04-10 14:52:09
+47714273,Penguin 'Toxicologists' Find PFAS Chemicals in Remote Patagonia,technology,71,25,giuliomagnifico,2026-04-10 14:52:09
+47714573,Artemis II and the invisible hazard on the way to the Moon,technology,58,42,zeristor,2026-04-10 14:52:09
+47718830,CPU-Z and HWMonitor Compromised,technology,10,0,Wingy,2026-04-10 14:52:10
+47708697,"Show HN: QVAC SDK, a universal JavaScript SDK for building local AI applications",technology,18,4,qvac,2026-04-10 14:52:10
+47678844,Show HN: Marimo pair – Reactive Python notebooks as environments for agents,technology,57,10,manzt,2026-04-10 14:52:12
+47712656,We've raised $17M to build what comes after Git,technology,223,479,ellieh,2026-04-10 14:52:12
+47715339,Show HN: Keeper – embedded secret store for Go (help me break it),technology,47,26,babawere,2026-04-10 14:52:12
+47675906,Generative art over the years,technology,200,50,evakhoury,2026-04-10 14:52:13
+47686046,The Art of Risk Management (2017),technology,35,11,walterbell,2026-04-10 14:52:13
+47709158,Charcuterie – Visual similarity Unicode explorer,technology,276,64,rickcarlino,2026-04-10 14:52:14
+47680005,RAM Has a Design Flaw from 1966. I Bypassed It [video],technology,322,112,surprisetalk,2026-04-10 14:52:14
+47707477,Old laptops in a colo as low cost servers,technology,350,205,argentum47,2026-04-10 14:52:15
+47706140,Unfolder for Mac – A 3D model unfolding tool for creating papercraft,technology,285,55,codazoda,2026-04-10 14:52:15
+47707632,"Instant 1.0, a backend for AI-coded apps",technology,186,92,stopachka,2026-04-10 14:52:16
+47710466,Kagi Product Tips – Customize Your Search Results with URL Redirects,technology,143,29,treetalker,2026-04-10 14:52:16
+47706141,Research-Driven Agents: When an agent reads before it codes,technology,196,51,hopechong,2026-04-10 14:52:16
+47708041,PicoZ80 – Drop-In Z80 Replacement,technology,214,33,rickcarlino,2026-04-10 14:52:17
+47674950,An AI robot in my home,technology,61,22,kukanani,2026-04-10 14:52:17
+47713113,"The Raft consensus algorithm explained through ""Mean Girls"" (2019)",technology,113,25,vermilingua,2026-04-10 14:52:17
+47709130,Reverse engineering Gemini's SynthID detection,technology,167,52,_tk_,2026-04-10 14:52:18
+47707762,"Hegel, a universal property-based testing protocol and family of PBT libraries",technology,125,38,PaulHoule,2026-04-10 14:52:19
+47718458,Tech job relocation market is recovering. The competition is growing faster,technology,8,0,andrewstetsenko,2026-04-10 14:52:19
+47697870,LittleSnitch for Linux,technology,1329,439,pluc,2026-04-10 14:52:19
+47675731,Principles of Mechanical Sympathy,technology,73,18,zdw,2026-04-10 14:52:20
+47713584,"Afrika Bambaataa, hip-hop pioneer, has died",technology,168,50,mellosouls,2026-04-10 14:52:20
+47665890,War on Raze,technology,23,14,tosh,2026-04-10 14:52:20
+47705413,Show HN: I built a Cargo-like build tool for C/C++,technology,158,142,randerson_112,2026-04-10 14:52:21
+47691730,I ported Mac OS X to the Nintendo Wii,technology,1877,317,blkhp19,2026-04-10 14:52:21
+47702541,A WebGPU implementation of Augmented Vertex Block Descent,technology,145,22,juretriglav,2026-04-10 14:52:22
+47660911,Will I ever own a zettaflop?,technology,114,80,surprisetalk,2026-04-10 14:52:22
+47710007,Moving from WordPress to Jekyll (and static site generators in general),technology,90,52,rgrieselhuber,2026-04-10 14:52:22
+47716663,White House staff told not to place bets on prediction markets,technology,117,69,chrischapman,2026-04-10 14:52:23
+47689550,"LLM plays an 8-bit Commander X16 game using structured ""smart senses""",technology,26,4,russellharper,2026-04-10 14:52:23
+47717571,Jennifer Aniston and Friends Cost Us 377GB and Broke Ext4 Hardlinks,technology,19,10,speckx,2026-04-10 14:52:23
+47683042,VFX HQ: Visual Effects Headquarters (2000),technology,15,0,exvi,2026-04-10 14:52:24
+47670188,Robots eat cars,technology,64,78,JMill,2026-04-10 14:52:24
